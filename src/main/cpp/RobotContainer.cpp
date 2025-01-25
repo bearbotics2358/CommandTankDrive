228dd539f7f6e85418ffa47f044e92c645979074
hdjwis <adarshyraju@gmail.com>
2025-01-24 18:58:50 -0600

    changed controls

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -13,8 +13,14 @@ RobotContainer::RobotContainer() {
 
 void RobotContainer::ConfigureBindings() {
   // Configure your trigger bindings here
-  m_driverController.LeftStick().WhileTrue(m_driveSubsystem.Move(0.2));
-  m_driverController.RightStick().WhileTrue(m_driveSubsystem.Turn(0.2));
-  m_driverController.LeftStick().WhileFalse(m_driveSubsystem.Move(0.0));
-  m_driverController.RightStick().WhileFalse(m_driveSubsystem.Turn(0.0));
+  m_driverController.LeftTrigger().WhileTrue(m_driveSubsystem.Move(0.5));
+  m_driverController.RightTrigger().WhileTrue(m_driveSubsystem.Move(-0.5));
+  m_driverController.LeftTrigger().OnFalse(m_driveSubsystem.Move(0));
+  m_driverController.RightTrigger().OnFalse(m_driveSubsystem.Move(0));
+  m_driverController.LeftBumper().WhileTrue(m_driveSubsystem.Turn(0.5));
+  m_driverController.RightBumper().WhileTrue(m_driveSubsystem.Turn(-0.5));
+  m_driverController.LeftBumper().OnFalse(m_driveSubsystem.Turn(0));
+  m_driverController.RightBumper().OnFalse(m_driveSubsystem.Turn(0));
+//  (m_driverController.LeftStick() && m_driverController.RightStick()).WhileFalse(m_driveSubsystem.Move(0));
+  m_driverController.X().WhileTrue(m_driveSubsystem.Move(0));
 };
